c819f7dddd777205dfc2bf8d9b65715360f0e6ef
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-09-18 20:58:17 -0300

    added barra de progreso

--- pdata/page/index.js
+++ pdata/page/index.js
@@ -1,93 +1,12 @@
+import GenresGraph from "./genresGraph.js"
+import ProgressBar from "./progressBar.js";
+
 async function main() {
     let res = await fetch("../data.json");
     if (res.ok) {
         let fullData = await res.json();
-        let genresData = {
-            totales:{},
-            vistos:{}
-        }
-        for (const series of fullData) {
-            for (const season of series.temporadas) {
-                for (const genre of series.generos) {
-                    if (typeof genresData.totales[genre] == 'undefined') {
-                        genresData.totales[genre] = 0
-                    }
-                    if (typeof genresData.vistos[genre] == 'undefined') {
-                        genresData.vistos[genre] = 0
-                    }
-                    genresData.totales[genre] += season.capitulos * season.duracion
-                    genresData.vistos[genre] += season.capitulosVistos * season.duracion
-                }
-            }
-        }
-        let genreDataForD3 = []
-        let maxValuesGenreData = {
-            vistos:0,
-            totales:0
-        }
-        for (const g in genresData.totales) {
-            genreDataForD3.push({name:g,vistos:genresData.vistos[g],totales:genresData.totales[g]})
-            if(genresData.vistos[g] > maxValuesGenreData.vistos){
-                maxValuesGenreData.vistos = genresData.vistos[g];
-            }
-            if(genresData.totales[g] > maxValuesGenreData.totales){
-                maxValuesGenreData.totales = genresData.totales[g];
-            }
-        }
-        genreDataForD3.sort(function(d1,d2) {
-            return d2.vistos - d1.vistos
-        })
-        genreDataForD3 = genreDataForD3.slice(0,10)
-
-        let tooltip = d3   .select("body")
-                            .append("div")
-                            .style("position", "fixed")
-                            .style("visibility", "hidden")
-                            .style("background-color", "white")
-                            .style("border", "solid")
-                            .style("border-width", "1px")
-                            .style("border-radius", "5px")
-                            .style("padding", "10px");
-
-
-
-        let divs = d3   .select("#genresGraph")
-                        .selectAll("div")
-                        .data(genreDataForD3)
-                        .enter()
-                        .append("div")
-        
-        divs            .style("position","relative")
-                        .style("height","3em")
-                        .style("display","flex")
-                        .style("margin-bottom",".1em")
-
-        divs            .append("p")
-                        .text(function(d) {
-                            return d.name
-                        })
-                        .style("position","absolute")
-                        .style("top","0")
-                        .style("left","1em")
-
-        divs            .append("div")
-                        .style("width",function(d) {
-                            return (d.vistos / maxValuesGenreData.totales) * 100+ "%"
-                        })
-                        .style("background-color","red")
-                        .on("mouseover", function(event,d,i){return tooltip.text(`Vistos: ${d.vistos} m`).style("visibility", "visible");})
-                        .on("mousemove", function(event){return tooltip.style("top", (event.pageY)+"px").style("left",(event.pageX)+"px");})
-                        .on("mouseout", function(event){return tooltip.style("visibility", "hidden");});
-
-        divs            .append("div")
-                        .style("width",function(d) {
-                            return ((d.totales - d.vistos) / maxValuesGenreData.totales) * 100+ "%"
-                        })
-                        .style("background-color","grey")
-                        .on("mouseover", function(event,d,i){return tooltip.text(`Por ver: ${d.totales - d.vistos} m`).style("visibility", "visible");})
-                        .on("mousemove", function(event){return tooltip.style("top", (event.pageY)+"px").style("left",(event.pageX)+"px");})
-                        .on("mouseout", function(event){return tooltip.style("visibility", "hidden");});
-
+        GenresGraph.appendGraph(fullData, d3.select("#genresGraph"))
+        ProgressBar.appendGraph(fullData,d3.select("#progressBar"))
     }
 }
 

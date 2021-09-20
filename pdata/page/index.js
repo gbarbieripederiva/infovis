8b0ed231b8b56acad25cc899a6466b37063b5f28
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-09-20 01:59:17 -0300

    added a series graph

--- pdata/page/index.js
+++ pdata/page/index.js
@@ -1,12 +1,14 @@
-import GenresGraph from "./genresGraph.js"
+import GenresGraphTime from "./genresGraphTime.js"
 import ProgressBar from "./progressBar.js";
+import GenresGraphSeries from "./genresGraphSeries.js";
 
 async function main() {
     let res = await fetch("../data.json");
     if (res.ok) {
         let fullData = await res.json();
-        GenresGraph.appendGraph(fullData, d3.select("#genresGraph"))
+        GenresGraphTime.appendGraph(fullData, d3.select("#genresGraphTime"))
         ProgressBar.appendGraph(fullData,d3.select("#progressBar"))
+        GenresGraphSeries.appendGraph(fullData,d3.select("#genresGraphSeries"))
     }
 }
 

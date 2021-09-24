23277bf59fef993dd5e04ea6d1287d5eaf45182c
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-09-23 21:17:52 -0300

    added a full list and made some update/additions to data.json

---
+++ pdata/page/allAnime/index.js
@@ -0,0 +1,45 @@
+function appendCard(div, data, i) {
+    let card = d3.select(div).classed("card", true);
+    card.append("h4").append("a").text(data.nombreOriginal).attr("href",data.temporadas[0].url);
+    card.append("img").attr("src",data.imageUrl);
+    let duracion = 0;
+    let visto = 0;
+    for (const temporada of data.temporadas) {
+        duracion += temporada.capitulos * temporada.duracion;
+        visto += temporada.capitulosVistos * temporada.duracion;
+    }
+    let progressSVG = card.append("svg");
+    progressSVG.attr("viewBox","0 0 10 1");
+    progressSVG .append("rect")
+                .attr("fill","rgb(235, 96, 96)")
+                .attr("x",0)
+                .attr("y",0)
+                .attr("width",(visto/duracion) * 10)
+                .attr("height",1);
+    progressSVG .append("rect")
+                .attr("fill","rgb(171, 171, 171)")
+                .attr("x",(visto/duracion) * 10)
+                .attr("y",0)
+                .attr("width",(1 - (visto/duracion)) * 10)
+                .attr("height",1);
+}
+
+async function main() {
+    let res = await fetch("../../data.json");
+    if (res.ok) {
+        let fullData = await res.json();
+        let data = fullData;
+        // data = data.slice(0, 10);
+        data = data.sort((a,b)=>a.nombreOriginal.localeCompare(b.nombreOriginal))
+        d3.select("#allAnimeCards")
+            .selectAll("div")
+            .data(data)
+            .enter()
+            .append("div")
+            .data(data)
+            .each(function (d, i) {
+                appendCard(this, d, i);
+            });
+    }
+}
+main();
